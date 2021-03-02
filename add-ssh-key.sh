89cbd073adb43c77fcc914700a08a5f0fa9ed93b
Quentin Castel <quentin.castel@yapily.com>
2021-03-02 15:24:43 +0000

    Get latest change from my repo

--- add-ssh-key.sh
+++ add-ssh-key.sh
@@ -3,9 +3,21 @@ set -e
 eval $(ssh-agent -s)
 
 echo "Do a SSH add with the key under env 'SSH_PRIVATE_KEY'"
-mkdir -p ~/.ssh/
-echo "$SSH_PRIVATE_KEY" | base64 -d > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+mkdir -p ${SSH_ROOT_FOLDER}/
+echo "$SSH_PRIVATE_KEY" | base64 -d > ${SSH_ROOT_FOLDER}/id_rsa
+chmod 600 ${SSH_ROOT_FOLDER}/id_rsa
+
 
 echo "Set StrictHostKeyChecking no"
-echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ${SSH_ROOT_FOLDER}/config
+
+#Whitelist some well-known git repositories management
+ssh-keyscan -t rsa github.com >> ${SSH_ROOT_FOLDER}/known_hosts
+ssh-keyscan -t rsa gitlab.com >> ${SSH_ROOT_FOLDER}/known_hosts
+ssh-keyscan -t rsa bitbucket.org >> ${SSH_ROOT_FOLDER}/known_hosts
+
+# Add an extra one on demand
+if [[ -n "${SSH_EXTRA_KNOWN_HOST}" ]]; then
+  echo "Whitelist  ${SSH_EXTRA_KNOWN_HOST} in ${SSH_ROOT_FOLDER}/known_hosts"
+  ssh-keyscan -t rsa ${SSH_EXTRA_KNOWN_HOST} >> ${SSH_ROOT_FOLDER}/known_hosts
+fi
