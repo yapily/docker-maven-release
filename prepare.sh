60a00f3b65358661c4392f0a5a431bf1922035e7
Quentin Castel <quentin.castel@yapily.com>
2021-06-09 16:40:05 +0100

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@ APP_VERSION=`echo "${APP_VERSION}" | sed -e "s/-SNAPSHOT//"`
 
 if [[ "$CI_COMMIT_BRANCH" == "hotfix" &&  "$APP_VERSION" == *0 ]]; then 
     echo "First commit on the hotfix branch, the version is actually .1"
-    APP_VERSION=`echo "${APP_VERSION}" | sed -e "s/.0/.1/"`
+    APP_VERSION=`echo "${APP_VERSION}" | sed -e "s/\.0/.1/"`
 fi
 echo "$APP_VERSION version in $CI_COMMIT_BRANCH  branch"
 
